a15330e30262d19c1a0b4a3a982468bec79eff68
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-08-19 10:51:18 +0530

    filtering user's task

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -21,6 +22,13 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'base/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        return context
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
