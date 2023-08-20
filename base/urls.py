120afd2022866699dc78a5dcf6b29796a5beed6b
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-08-20 23:09:01 +0530

    link register page to urls

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', CustomLogin.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    path('register/', RegisterUser.as_view(), name = 'register'),
     path('', TaskList.as_view(), name = 'index'),
     path('task/<str:pk>/', TaskDetail.as_view(), name = 'taskdetail' ),
     path('task-create', TaskCreate.as_view(), name = 'task-create'),
