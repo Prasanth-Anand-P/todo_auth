fbcfef89d4b050f6c2c9bbb54e36fc88b3c9d5e9
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-09-09 22:18:39 +0530

    hosting stuffs

--- todo_auth/settings.py
+++ todo_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z8*@+na@egza)*qf)*+6rd2d(p_s9jr)bmb-t@dnr^zjdtye^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
