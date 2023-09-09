c5eecdef99f8e8ac701715d47e708b774852f21f
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-09-09 23:55:47 +0530

    changes

--- todo_auth/settings.py
+++ todo_auth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z8*@+na@egza)*qf)*+6rd2d(p_s9jr)bmb-t@dnr^zjdtye^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
